8878d1dfbb65835eb3cc9cb95f336df7256c1e74
jong-k <fully.charged07@gmail.com>
2025-05-20 16:32:42 +0900

    feat: layout 컴포넌트 class 에 cn 함수 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppHeader from "../components/common/AppHeader";
 import { Toaster } from "@/components/ui/sonner";
-
+import { cn } from "@/lib/utils";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,10 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="ko">
+      <body className={cn(geistSans.variable, geistMono.variable, "antialiased")}>
         <AppHeader />
-        {children}
+        <main className={cn("px-8", "py-16")}>{children}</main>
         <Toaster />
       </body>
     </html>
