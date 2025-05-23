a93bedcfce60f2bf30dc5e4696be062138280c32
jong-k <fully.charged07@gmail.com>
2025-05-23 16:08:08 +0900

    feat: page wrapper 가 mdx wrapper를 래핑하게 수정

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,25 +1,10 @@
 import PageWrapper from "@/components/common/PageWrapper";
-import { cn } from "@/lib/utils";
+import MdxWrapper from "@/components/common/MdxWrapper";
 
 export default function PostsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className={cn(
-        "prose",
-        "prose-headings:mt-8",
-        "prose-headings:font-semibold",
-        "prose-headings:text-black",
-        "dark:prose-headings:text-white",
-        "prose-h1:text-5xl",
-        "prose-h2:text-4xl",
-        "prose-h3:text-3xl",
-        "prose-h4:text-2xl",
-        "prose-h5:text-xl",
-        "prose-h6:text-lg",
-        "prose-li:marker:text-black",
-      )}
-    >
-      <PageWrapper>{children}</PageWrapper>
-    </div>
+    <PageWrapper>
+      <MdxWrapper>{children}</MdxWrapper>
+    </PageWrapper>
   );
 }
