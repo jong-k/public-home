c5eabc316430a55855b65d4a1a6d06e46a215c16
jong-k <fully.charged07@gmail.com>
2025-05-23 12:29:00 +0900

    feat: page wrapper에 py 부여

--- src/components/common/PageWrapper.tsx
+++ src/components/common/PageWrapper.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
-  return <main className={cn("px-page", "mx-auto")}>{children}</main>;
+  return <main className={cn("px-page", "mx-auto", "py-20")}>{children}</main>;
 }
