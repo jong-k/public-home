93a8862f4fca4c15aa4f823b9a375591e3986a38
jong-k <fully.charged07@gmail.com>
2025-05-25 23:06:29 +0900

    feat: 반응형 padding 추가

--- src/app/posts/components/PostHeader.tsx
+++ src/app/posts/components/PostHeader.tsx
@@ -7,7 +7,14 @@ type PostHeaderProps = Omit<Frontmatter, "description">;
 export default function PostHeader({ category, title, date }: PostHeaderProps) {
   return (
     <section
-      className={cn("flex", "flex-col", "gap-4", "items-start", "sm:items-center")}
+      className={cn(
+        "flex",
+        "flex-col",
+        "gap-4",
+        "items-start",
+        "sm:items-center",
+        "sm:py-5",
+      )}
     >
       <PostCategoryBadge category={category} />
       <h2 className={cn("leading-[110%]", "font-bold", "text-post-title")}>{title}</h2>
