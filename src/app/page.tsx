04d217a57b4b59b5d04eab03f61d4221c212e156
jong-k <fully.charged07@gmail.com>
2025-05-28 23:49:05 +0900

    feat: 랜딩페이지에 게시글 메타데이터 리스트렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import PageWrapper from "@/components/common/PageWrapper";
-import PostCard from "./components/PostCard";
+// import PostCard from "./components/PostCard";
+import { readdir, readFile } from "fs/promises";
+import path from "path";
+import matter from "gray-matter";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await readdir(path.join(process.cwd(), "src/contents"));
+  const postFiles = posts.map((post) =>
+    path.join(process.cwd(), "src/contents", post, "index.mdx"),
+  );
+  const postMetadatas = await Promise.all(
+    postFiles.map(async (file) => {
+      const content = await readFile(file, "utf-8");
+      const { data } = matter(content);
+      return data;
+    }),
+  );
+
   return (
     <PageWrapper>
-      {Array.from({ length: 100 }).map((_, index) => (
-        <PostCard key={index} />
+      {postMetadatas.map((post) => (
+        <pre key={post.title}>{JSON.stringify(post, null, 2)}</pre>
       ))}
     </PageWrapper>
   );
