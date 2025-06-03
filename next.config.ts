fdb2c34ab11fa4ff4c08fd35b107f016d772dcc7
김종한 <fully.charged07@gmail.com>
2025-06-03 13:00:54 +0900

    chore: next config 설정 변경 (#43)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+  output: "export",
   pageExtensions: ["ts", "tsx", "mdx", "md"],
 };
 
