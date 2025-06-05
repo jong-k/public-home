ef21a23ce68b2471f46ef2fb08847fcca425c09d
jong-k <fully.charged07@gmail.com>
2025-06-05 14:12:45 +0900

    feat: 커스텀 404 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <div className={cn("flex", "h-screen", "flex-col", "items-center", "justify-center")}>
+      <h2 className={cn("text-2xl", "font-bold", "mb-4")}>
+        👾 페이지를 찾을 수 없습니다 👾
+      </h2>
+      <Button asChild>
+        <Link href="/">홈으로 돌아기기</Link>
+      </Button>
+    </div>
+  );
+}
