9083550e1b76c8aa44209326de3599e5c9579636
김종한 <fully.charged07@gmail.com>
2025-06-09 12:52:29 +0900

    feat: 게시글 각 페이지의 SEO (#56)
    * chore: git ignore 수정
    
    * chore: og image 작은 사이즈로 교체
    
    * chore: pwa 관련 manifest 제거
    
    * chore: 불필요 이미지 제거
    
    * feat: apple touch icon 추가
    
    * chore: 미사용 아이콘 제거
    
    * chore: 사이트맵 업데이트
    
    * feat: 페이지별 타이틀 동적 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,25 +10,28 @@ export const metadata: Metadata = {
   description: "프론트엔드 개발자 김종한의 기술 블로그입니다",
   icons: {
     icon: "/favicons/favicon.ico",
-    apple: "/favicons/apple-touch-icon.png",
     shortcut: "/favicons/favicon-32x32.png",
-    other: [{ rel: "manifest", url: "/favicons/site.webmanifest" }],
+    apple: "/favicons/apple-touch-icon.png",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "/favicons/apple-touch-icon.png",
+    },
   },
   openGraph: {
-    type: "website",
-    url: "https://kimjonghan.com/",
-    locale: "ko_KR",
     title: "김종한 기술 블로그",
     description: "프론트엔드 개발자 김종한의 기술 블로그입니다",
+    url: "https://kimjonghan.com/",
+    siteName: "김종한 기술 블로그",
     images: [
       {
-        url: "https://kimjonghan.com/images/og-thumbnail.png",
-        width: 1200,
-        height: 630,
+        url: "https://kimjonghan.com/images/opengraph-image.png",
+        width: 800,
+        height: 533,
         alt: "김종한 기술 블로그",
       },
     ],
-    siteName: "김종한 기술 블로그",
+    locale: "ko_KR",
+    type: "website",
   },
   twitter: {
     card: "summary_large_image",
@@ -36,9 +39,9 @@ export const metadata: Metadata = {
     description: "프론트엔드 개발자 김종한의 기술 블로그입니다",
     images: [
       {
-        url: "https://kimjonghan.com/images/og-thumbnail.png",
-        width: 1200,
-        height: 630,
+        url: "https://kimjonghan.com/images/opengraph-image.png",
+        width: 800,
+        height: 533,
         alt: "김종한 기술 블로그",
       },
     ],
