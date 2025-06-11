0dd1e9b6ab73833640e986e1c5229934160adaba
jong-k <fully.charged07@gmail.com>
2025-06-11 14:54:26 +0900

    feat: about page 추가

---
+++ src/constants/profile.ts
@@ -0,0 +1,10 @@
+export const EXTERNAL_LINKS = [
+  {
+    label: "GitHub",
+    href: "https://github.com/jong-k",
+  },
+  {
+    label: "티스토리 블로그(개인 기록용)",
+    href: "https://ggarden.tistory.com",
+  },
+];
