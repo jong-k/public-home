05d7d4887c863fd95b1ae3c01f87dc773192c67f
김종한 <fully.charged07@gmail.com>
2025-08-19 22:19:03 +0900

    feat: 마크다운 컨텐츠 스타일 개선 (#68)
    * feat: posts page background color 추가
    
    * feat: header, gnb에 posts page 일 때 background 변경 코드 추가
    
    * feat: posts 페이지에서 백그라운드 컬러 변경
    
    * feat: blockquote 내부의 쌍따옴표 제거
    
    * fix: 스타일 충돌 개선
    
    * chore: 주석 수정

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -2,5 +2,9 @@ import PageWrapper from "@/components/common/PageWrapper";
 import { cn } from "@/lib/utils";
 
 export default function PostsLayout({ children }: { children: React.ReactNode }) {
-  return <PageWrapper className={cn("max-w-page", "mx-auto")}>{children}</PageWrapper>;
+  return (
+    <div className={cn("bg-background-posts-page")}>
+      <PageWrapper className={cn("max-w-page", "mx-auto")}>{children}</PageWrapper>;
+    </div>
+  );
 }
